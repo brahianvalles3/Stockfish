65bba7909d3b82cf46c13b7c608784aa682efa06
brahianvalles3 <brahianvalles2@yahoo.es>
2015-02-02 17:38:30 -0430

    Update evaluate.cpp
    adpsd

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -162,7 +162,8 @@ namespace {
   const Score TrappedRook        = S(92,  0);
   const Score Unstoppable        = S( 0, 20);
   const Score Hanging            = S(31, 26);
-
+  const Score PawnAttackThreat   = S(28, 25);
+  
   // Penalty for a bishop on a1/h1 (a8/h8 for black) which is trapped by
   // a friendly pawn on b2/g2 (b7/g7 for black). This can obviously only
   // happen in Chess960 games.
